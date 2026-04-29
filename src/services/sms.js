03de82cf4757d3d8e02a47d61740f5c5eb6339b1
romeo Jacobs <red1@mac.home>
2026-04-28 22:33:00 -0400

    enable real SMS

--- src/services/sms.js
+++ src/services/sms.js
@@ -1,4 +1,4 @@
-const { Vonage } = require('@vonage/server-sdk');
+const Vonage = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
   apiKey: process.env.VONAGE_API_KEY,
@@ -10,12 +10,12 @@ const otpStore = {};
 const sendOTP = async (phone) => {
   const code = Math.floor(100000 + Math.random() * 900000).toString();
   otpStore[phone] = { code, expiresAt: Date.now() + 10 * 60 * 1000 };
-  console.log(`OTP for ${phone} is ${code}`);
-  try {
-    await vonage.sms.send({ to: phone, from: 'MayI', text: `Your May I code is: ${code}` });
-  } catch (err) {
-    console.log('Vonage response:', err);
-  }
+  await new Promise((resolve, reject) => {
+    vonage.message.sendSms('May I', phone, `Your May I verification code is: ${code}`, (err, responseData) => {
+      if (err) reject(err);
+      else resolve(responseData);
+    });
+  });
 };
 
 const verifyOTP = (phone, code) => {
